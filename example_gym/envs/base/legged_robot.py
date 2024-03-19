ed90510bc1a3762f8654b00f565c2ec22ec90a1c
dongjukim <dongjukim@naver.com>
2024-03-19 13:00:18 +0900

    eww

--- example_gym/envs/base/legged_robot.py
+++ example_gym/envs/base/legged_robot.py
@@ -10,8 +10,9 @@ from isaacgym import gymtorch, gymapi, gymutil
 
 from .base_task import BaseTask
 from .legged_robot_config import LeggedRobotCfg
+from example_gym.utils.helpers import class_to_dict
 
-
+from legged_gym.utils.math import quat_apply_yaw, wrap_to_pi
 from legged_gym.utils.terrain import Terrain
 class LeggedRobot(BaseTask):
     def __init__(self,cfg:LeggedRobotCfg,sim_params, physics_engine, sim_device, headless ):
@@ -78,10 +79,17 @@ class LeggedRobot(BaseTask):
         
         self.episode_length_buf +=1
         self.common_step_counter +=1
+        print(self.episode_length_buf)
 
         #prepare quantities
         self.base_quat[:] = self.root_states[:,3:7]
+        self.base_lin_vel = quat_rotate_inverse(self.base_quat, self.root_states[:, 7:10])
+        self.base_ang_vel[:] = quat_rotate_inverse(self.base_quat, self.root_states[:, 10:13])
+        self.projected_gravity[:] = quat_rotate_inverse(self.base_quat, self.gravity_vec)
 
+        self._post_physics_step_callback()
+
+        # compute observations, rewards, resets, ...
     def set_camera(self, position, lookat):
 
 
@@ -122,8 +130,19 @@ class LeggedRobot(BaseTask):
         self.p_gains = torch.zeros(self.num_actions,dtype=torch.float,device=self.device,requires_grad=False)
         self.d_gains = torch.zeros(self.num_actions,dtype=torch.float,device=self.device,requires_grad=False)
         self.gravity_vec = to_torch(get_axis_params(-1.,self.up_axis_idx),device=self.device).repeat((self.num_envs,1))
+        self.forward_vec = to_torch([1., 0., 0.], device=self.device).repeat((self.num_envs, 1))
+        self.commands = torch.zeros(self.num_envs, self.cfg.commands.num_commands, dtype=torch.float, device=self.device, requires_grad=False) # x vel, y vel, yaw vel, heading
 
 
+
+
+
+
+        self.base_lin_vel = quat_rotate_inverse(self.base_quat, self.root_states[:,7:10]) #global -> base
+        self.base_ang_vel = quat_rotate_inverse(self.base_quat, self.root_states[:,10:13]) # global -> base
+        self.projected_gravity = quat_rotate_inverse(self.base_quat,self.gravity_vec)
+
+        self.measured_heights = 0
 
         # joint positions offsets and PD gains
         self.default_dof_pos = torch.zeros(self.num_dof,dtype=torch.float,device=self.device,requires_grad=False)
@@ -146,7 +165,9 @@ class LeggedRobot(BaseTask):
         self.default_dof_pos = self.default_dof_pos.unsqueeze(0) # torch.shape(1,12) 나중에 연산을 수행하기 위해 차원을 추가하였음
 
 
-
+    def _parse_cfg(self,cfg):
+        self.dt = self.cfg.control.decimation * self.sim_params.dt
+        self.command_ranges = class_to_dict(self.cfg.commands.ranges)
     def _prepare_reward_function(self):
         
 
@@ -244,3 +265,53 @@ class LeggedRobot(BaseTask):
 
 
     def _create_trimesh(self):
+
+    #-------------------Callbacks------------------
+        
+
+    def _post_physics_step_callback(self):
+        """ Callback called before computing terminations, rewards, and observations
+            Default behaviour: Compute ang vel command based on target and heading, compute measured terrain heights and randomly push robots
+        """        
+        # episode_length_buf(tensor 형식) 에서 nonzero를 만족하는 tensor위치를 반환함
+        # env_ids shape --> torch.size([env_ids])
+        env_ids = (self.episode_length_buf % int(self.cfg.commands.resampling_time / self.dt)==0).nonzero(as_tuple=False).flatten()
+        self._resample_commands(env_ids)
+        if self.cfg.commands.heading_command:
+            forward = quat_apply(self.base_quat, self.forward_vec)
+            print(forward)
+            heading = torch.atan2(forward[:, 1], forward[:, 0])
+            self.commands[:, 2] = torch.clip(0.5*wrap_to_pi(self.commands[:, 3] - heading), -1., 1.)
+
+        if self.cfg.terrain.measure_heights:
+            self.measured_heights = self._get_heights()
+        if self.cfg.domain_rand.push_robots and  (self.common_step_counter % self.cfg.domain_rand.push_interval == 0):
+            self._push_robots()
+    # resampling time과 self.dt시간에 의해 자주 업데이트 됨 
+    def _resample_commands(self,env_ids):
+        """ Randommly select commands of some environments
+
+        Args:
+            env_ids (List[int]): Environments ids for which new commands are needed
+        """
+        #torch.size([env_number,cfg.num_commands]) 여기서는 num_commands : 4
+        self.commands[env_ids, 0] = torch_rand_float(self.command_ranges["lin_vel_x"][0], self.command_ranges["lin_vel_x"][1], (len(env_ids), 1), device=self.device).squeeze(1)
+        self.commands[env_ids, 1] = torch_rand_float(self.command_ranges["lin_vel_y"][0], self.command_ranges["lin_vel_y"][1], (len(env_ids), 1), device=self.device).squeeze(1)
+        if self.cfg.commands.heading_command:
+            self.commands[env_ids, 3] = torch_rand_float(self.command_ranges["heading"][0], self.command_ranges["heading"][1], (len(env_ids), 1), device=self.device).squeeze(1)
+        else:
+            self.commands[env_ids, 2] = torch_rand_float(self.command_ranges["ang_vel_yaw"][0], self.command_ranges["ang_vel_yaw"][1], (len(env_ids), 1), device=self.device).squeeze(1)
+
+        # set small commands to zero
+        # 참이면 1이 나오고 (true) 거짓이면(0)이 나와서 0.2보다 작은 값들은 모두 0으로 마스킹 됨
+        self.commands[env_ids, :2] *= (torch.norm(self.commands[env_ids, :2], dim=1) > 0.2).unsqueeze(1)
+
+    def _push_robots(self):
+        """ Random pushes the robots. Emulates an impulse by setting a randomized base velocity. 
+        """
+
+
+
+
+
+    def _get_heights(self,env_ids=None):
--- example_gym/envs/base/legged_robot_config.py
+++ example_gym/envs/base/legged_robot_config.py
@@ -16,7 +16,18 @@ class LeggedRobotCfg(BaseConfig):
         vertical_sacle = 0.005 # [m]
         border_size = 25 # [m]
         curriculum = True
+        #rough terrain only:
+        measure_heights = True
 
+    class commands:
+        num_commands = 4 # default: lin_vel_x, lin_vel_y, ang_vel_yaw, heading (in heading mode ang_vel_yaw is recomputed from heading error)
+        resampling_time = 10. # time before command are changed[s] 이 값이 클수록 lin_vel_x, lin_vel_y, ang_vel_yaw, heading 값이 자주 바뀜
+        heading_command = True # if True: compute ang vel command from heading error
+        class ranges:
+            lin_vel_x = [-1.0, 1.0] # min max [m/s]
+            lin_vel_y = [-1.0, 1.0]   # min max [m/s]
+            ang_vel_yaw = [-1, 1]    # min max [rad/s]
+            heading = [-3.14, 3.14]
 
     class control:
         control_type = 'p' # p : POSTION , V:velocity, T:Torques
@@ -32,6 +43,7 @@ class LeggedRobotCfg(BaseConfig):
         file =""
         name = "legged_robot"
         foot_name = "None" # name of the feet bodies, used to index body state and contact force tensors
+
     class init_state:
         pos = [0.0,0.0,1.] # x y z (m)
         rot = [0.0,0.0,0.0,1.0] # x y z w [quat]
@@ -47,6 +59,14 @@ class LeggedRobotCfg(BaseConfig):
 
         clip_actions = 100.
         clip_observations = 100.
+
+    class domain_rand:
+        randomize_friction = True
+        friction_range = [0.5, 1.25]
+        randomize_base_mass = False
+        added_mass_range = [-1., 1.]
+        push_robots = True
+        push_interval_s = 15
 
 
 
